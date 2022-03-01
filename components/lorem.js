b1a05d0de011f2fbb6a3e8a812f96c663b7d827d
Cassidy Smith <cassidysmith319@gmail.com>
2022-03-01 01:24:46 -0600

    fixx target _blanks

--- components/lorem.js
+++ components/lorem.js
@@ -4,21 +4,21 @@ export default function Lorem() {
 
   return (
     <>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quod ea non occurrentia fingunt, vincunt Aristonem; Similiter sensus, cum accessit ad naturam, tuetur illam quidem, sed etiam se tuetur; Philosophi autem in suis lectulis plerumque moriuntur. <b>Certe, nisi voluptatem tanti aestimaretis.</b> Duo Reges: constructio interrete. <a href="http://loripsum.net/" target="_blank">Non semper, inquam;</a> </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quod ea non occurrentia fingunt, vincunt Aristonem; Similiter sensus, cum accessit ad naturam, tuetur illam quidem, sed etiam se tuetur; Philosophi autem in suis lectulis plerumque moriuntur. <b>Certe, nisi voluptatem tanti aestimaretis.</b> Duo Reges: constructio interrete. <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Non semper, inquam;</a> </p>
 
       <h6>Nondum autem explanatum satis, erat, quid maxime natura vellet.</h6>
 
-      <p>Universa enim illorum ratione cum tota vestra confligendum puto. Quae diligentissime contra Aristonem dicuntur a Chryippo. Addidisti ad extremum etiam indoctum fuisse. <i>Nescio quo modo praetervolavit oratio.</i> Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus; <a href="http://loripsum.net/" target="_blank">Sed ad rem redeamus;</a> <b>Aliter enim explicari, quod quaeritur, non potest.</b> </p>
+      <p>Universa enim illorum ratione cum tota vestra confligendum puto. Quae diligentissime contra Aristonem dicuntur a Chryippo. Addidisti ad extremum etiam indoctum fuisse. <i>Nescio quo modo praetervolavit oratio.</i> Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus; <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Sed ad rem redeamus;</a> <b>Aliter enim explicari, quod quaeritur, non potest.</b> </p>
 
-      <p><mark>Dat enim intervalla et relaxat.</mark> <a href="http://loripsum.net/" target="_blank">Nemo igitur esse beatus potest.</a> <b>Sed tamen intellego quid velit.</b> Ita fit cum gravior, tum etiam splendidior oratio. </p>
+      <p><mark>Dat enim intervalla et relaxat.</mark> <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Nemo igitur esse beatus potest.</a> <b>Sed tamen intellego quid velit.</b> Ita fit cum gravior, tum etiam splendidior oratio. </p>
 
       <h3>Haec quo modo conveniant, non sane intellego.</h3>
 
-      <p>Non quam nostram quidem, inquit Pomponius iocans; <a href="http://loripsum.net/" target="_blank">Comprehensum, quod cognitum non habet?</a> Sed ea mala virtuti magnitudine obruebantur. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Hoc etsi multimodis reprehendi potest, tamen accipio, quod dant. Qui autem diffidet perpetuitati bonorum suorum, timeat necesse est, ne aliquando amissis illis sit miser. Est igitur officium eius generis, quod nec in bonis ponatur nec in contrariis. Nec vero alia sunt quaerenda contra Carneadeam illam sententiam. </p>
+      <p>Non quam nostram quidem, inquit Pomponius iocans; <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Comprehensum, quod cognitum non habet?</a> Sed ea mala virtuti magnitudine obruebantur. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Hoc etsi multimodis reprehendi potest, tamen accipio, quod dant. Qui autem diffidet perpetuitati bonorum suorum, timeat necesse est, ne aliquando amissis illis sit miser. Est igitur officium eius generis, quod nec in bonis ponatur nec in contrariis. Nec vero alia sunt quaerenda contra Carneadeam illam sententiam. </p>
 
       <h2>Te enim iudicem aequum puto, modo quae dicat ille bene noris.</h2>
 
-      <p>Mihi vero, inquit, placet agi subtilius et, ut ipse dixisti, pressius. <a href="http://loripsum.net/" target="_blank">Quorum sine causa fieri nihil putandum est.</a> Quasi ego id curem, quid ille aiat aut neget. Odium autem et invidiam facile vitabis. Philosophi autem in suis lectulis plerumque moriuntur. Quae similitudo in genere etiam humano apparet. Quam illa ardentis amores excitaret sui! Cur tandem? Quamquam te quidem video minime esse deterritum. Traditur, inquit, ab Epicuro ratio neglegendi doloris. </p>
+      <p>Mihi vero, inquit, placet agi subtilius et, ut ipse dixisti, pressius. <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Quorum sine causa fieri nihil putandum est.</a> Quasi ego id curem, quid ille aiat aut neget. Odium autem et invidiam facile vitabis. Philosophi autem in suis lectulis plerumque moriuntur. Quae similitudo in genere etiam humano apparet. Quam illa ardentis amores excitaret sui! Cur tandem? Quamquam te quidem video minime esse deterritum. Traditur, inquit, ab Epicuro ratio neglegendi doloris. </p>
 
       <ol>
         <li>Inquit, dasne adolescenti veniam?</li>
@@ -50,7 +50,7 @@ export default function Lorem() {
 
       <p>Multa sunt dicta ab antiquis de contemnendis ac despiciendis rebus humanis; Non ego tecum iam ita iocabor, ut isdem his de rebus, cum L. Quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? Qui autem esse poteris, nisi te amor ipse ceperit? Hoc dixerit potius Ennius: Nimium boni est, cui nihil est mali. Non risu potius quam oratione eiciendum? Atque ego: Scis me, inquam, istud idem sentire, Piso, sed a te opportune facta mentio est. </p>
 
-      <p>Verba tu fingas et ea dicas, quae non sentias? <i>Itaque ad tempus ad Pisonem omnes.</i> Et quidem illud ipsum non nimium probo et tantum patior, philosophum loqui de cupiditatibus finiendis. <a href="http://loripsum.net/" target="_blank">Conferam tecum, quam cuique verso rem subicias;</a> Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. </p>
+      <p>Verba tu fingas et ea dicas, quae non sentias? <i>Itaque ad tempus ad Pisonem omnes.</i> Et quidem illud ipsum non nimium probo et tantum patior, philosophum loqui de cupiditatibus finiendis. <a href="http://loripsum.net/" target="_blank" rel="noreferrer">Conferam tecum, quam cuique verso rem subicias;</a> Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. </p>
 
       <ol>
         <li>Legimus tamen Diogenem, Antipatrum, Mnesarchum, Panaetium, multos alios in primisque familiarem nostrum Posidonium.</li>
@@ -62,7 +62,8 @@ export default function Lorem() {
 
       <h5>Itaque hic ipse iam pridem est reiectus;</h5>
 
-      <p>Quod cum ita sit, perspicuum est omnis rectas res atque laudabilis eo referri, ut cum voluptate vivatur. Unum est sine dolore esse, alterum cum voluptate. <i>Sin aliud quid voles, postea.</i> <a href="http://loripsum.net/" target="_blank">Quicquid porro animo cernimus, id omne oritur a sensibus;</a> Ex ea difficultate illae fallaciloquae, ut ait Accius, malitiae natae sunt. Quodsi ipsam honestatem undique pertectam atque absolutam. </p>
+      <p>Quod cum ita sit, perspicuum est omnis rectas res atque laudabilis eo referri, ut cum voluptate vivatur. Unum est sine dolore esse, alterum cum voluptate. <i>Sin aliud quid voles, postea.</i> <a href="h
+      ttp://loripsum.net/" target="_blank">Quicquid porro animo cernimus, id omne oritur a sensibus;</a> Ex ea difficultate illae fallaciloquae, ut ait Accius, malitiae natae sunt. Quodsi ipsam honestatem undique pertectam atque absolutam. </p>
     </>
   )
 }
