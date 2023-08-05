0c5f37419014271ec8f4720c525373820847466e
Cassidy Smith <cassidysmith319@gmail.com>
2023-08-05 17:10:30 -0500

    add image placeholder for cactus and button to regenerate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService(),
+  },
 });
---
+++ src/components/PricklyPearController.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { generateCactus } from 'prickly-pear'
+  let canvasElement
+  let imagePlaceholderWrapper
+
+  async function click() {
+    generateCactus(canvasElement, { color: '#31977b' })
+    imagePlaceholderWrapper.setAttribute('hidden', '')
+  }
+
+</script>
+
+<canvas class="prickly-pear" bind:this={canvasElement} />
+
+<div class="image-placeholder-wrapper" bind:this={imagePlaceholderWrapper}>
+  <slot name="placeholder-cactus"/>
+</div>
+
+<div class="controls-row">
+  <button on:click={click}>Grow Random Prickly Pear</button>
+</div>
+
+
+<style lang="scss">
+  .prickly-pear {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100% !important;
+    height: 100% !important;
+
+
+  }
+
+  :global(.prickly-pear ~ astro-slot > img) {
+    display: none;
+  }
+
+  .controls-row {
+    z-index: 1;
+    padding: var(--space-2);
+    position: relative;
+    bottom: 0;
+  }
+</style>
